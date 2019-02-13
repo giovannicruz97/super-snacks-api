96e1923c538e1d545f1692fa3f16ef5cb63d9981
Giovanni Cruz <giovanni.cruz97@hotmail.com>
2019-02-13 19:18:10 -0200

    Atualizado testes de MachineController

--- test/controllers/MachineController.test.js
+++ test/controllers/MachineController.test.js
@@ -14,33 +14,31 @@ let emptyFakeMachine = {
   hash: ''
 };
 
-afterAll(async () => await Machine.findByIdAndRemove(fakeMachine._id));
+describe('Testa a criação de Máquinas', () => {
+  it('Tenta criar uma nova máquina', async done => {
+    let response = await request(app.callback())
+      .post('/machines')
+      .send(fakeMachine);
+    expect(response.status).toEqual(200);
+    fakeMachine._id = response.body.data.machine._id;
+    done();
+  });
 
-describe('Testa o MachineController', () => {
-  describe('Testa a criação de Máquinas', () => {
-    it('Tenta criar uma nova máquina', async done => {
-      let response = await request(app.callback())
-        .post('/machines')
-        .send(fakeMachine);
-      expect(response.status).toEqual(200);
-      fakeMachine._id = response.body.data.machine._id;
-      done();
-    });
+  it('Tenta criar uma máquina com informações existentes', async done => {
+    let response = await request(app.callback())
+      .post('/machines')
+      .send(fakeMachine);
+    expect(response.status).toEqual(400);
+    done();
+  });
 
-    it('Tenta criar uma máquina com informações existentes', async done => {
-      let response = await request(app.callback())
-        .post('/machines')
-        .send(fakeMachine);
-      expect(response.status).toEqual(400);
-      done();
-    });
-
-    it('Tenta criar uma máquina com valores vazios', async done => {
-      let response = await request(app.callback())
-        .post('/machines')
-        .send(emptyFakeMachine);
-      expect(response.status).toEqual(400);
-      done();
-    });
+  it('Tenta criar uma máquina com valores vazios', async done => {
+    let response = await request(app.callback())
+      .post('/machines')
+      .send(emptyFakeMachine);
+    expect(response.status).toEqual(400);
+    done();
   });
 });
+
+afterAll(async () => await Machine.deleteOne({ _id: fakeMachine._id }));
